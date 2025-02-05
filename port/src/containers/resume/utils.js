0a3544c72d9a090b80ac6005790fb1780c77c6a8
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-05 20:40:18 +0300

    first

---
+++ port/src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    experience: [
+      {
+        title: "Consultant",
+        subTitle: "Miami, FL",
+        description:
+          " User Experience, Visual Design",
+      },
+      {
+        title: "Senior Consultant",
+        subTitle: "ethiopia, bahirdar",
+        description:
+          "Web development, Databases, Backend developement",
+      },
+      {
+        title: "Project Manager",
+        subTitle: "bongs, ethiopia",
+        description:
+          "Creative Direction Project Management, Team Leading",
+      },
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "Tana haik ",
+        description:
+          "10th Grade",
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "Tanahaik secondey schhol",
+        description:
+          "12th Grade in Science",
+      },
+      {
+        title: "B.Tech",
+        subTitle: "Bonga universty",
+        description:
+          "Bachelor of degree  in computer science",
+      },
+    ],
+  };
---
+++ port/src/containers/skills/utils.js
@@ -0,0 +1,53 @@
+export const skillsData = [
+    {
+      label: "FRONT END",
+      data: [
+        {
+          skillName: "react",
+          percentage: "90",
+        },
+        {
+          skillName: "tailwind css",
+          percentage: "50",
+        },
+        {
+          skillName: "JAVASCRIPT",
+          percentage: "80",
+        },
+      ],
+    },
+    {
+      label: "BACK END",
+      data: [
+        {
+          skillName: "nodejs",
+          percentage: "70",
+        },
+        {
+          skillName: "PYTHON",
+          percentage: "65",
+        },
+        {
+          skillName: "React",
+          percentage: "35",
+        },
+      ],
+    },
+    {
+      label: "DATABASES",
+      data: [
+        {
+          skillName: "SQL",
+          percentage: "70",
+        },
+        {
+          skillName: "MONGO DB",
+          percentage: "65",
+        },
+        {
+          skillName: "Nosql",
+          percentage: "85",
+        },
+      ],
+    },
+  ];
