422429a6ed3e44b6939e485fdb2558295c1c863e
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-08 09:52:28 +0300

    portfolio

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./containers/home";
 import About from "./containers/about";
 import Resume from "./containers/resume";
 import Skills from "./containers/skills";
-import Portfolio from "./containers/portfolio";
+
 import Contact from "./containers/contact";
 import Navbar from "./components/navBar";
 import particles from "./utils.js/particles";
@@ -39,7 +39,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/skills" element={<Skills />} />
-        <Route path="/portfolio" element={<Portfolio />} />
+       
         <Route path="/contact" element={<Contact />} />
       </Routes>
       </div>
